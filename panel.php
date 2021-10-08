45c1a4e93f976c80f03369e7e283e61c834388a6
crushtkd <crush.tkd@gmail.com>
2021-10-08 04:28:23 -0300

    login

---
+++ panel.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "logeado con exito";
+
+
+?>
--- procesarlogin.php
+++ procesarlogin.php
@@ -1,11 +1,25 @@
 <?php 
 
+// incluyo el archivo de coneccion
+include_once ('conexion.php');
 
+// asigno la funcion a una variable
+$conn = conexion();
 
+$usuario = $_POST['usuario'];
+$pass = $_POST['contrasena'];
 
-$_POST['usuario'];
-$_POST['contrasena'] 
+$clave =  password_hash($pass, PASSWORD_DEFAULT);
 
+$sql = "SELECT usuario, pass FROM usuarios where usuario = '$usuario' and pass = '$clave'";
+$result=mysqli_query($conexion,$sql);
+
+if(mysqli_num_rows($result) > 0){  /* realiza verificacion en tuplas, buscando coincidencia de los registros*/
+    $_SESSION['user']=$usuario;
+    header('Location: panel.php');
+}else{
+    header('Location: login.php');
+}
 
 
 
